e797c448086abeb034db1882fb6dfe231f09ece6
Ishan <dedeishan19920@gmail.com>
2023-06-17 18:59:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+let int = setInterval(blurring, 20);
+function blurring() {
+  load++;
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, min_in, max_in, min_out, max_out) => {
+  return ((num - min_in) * (max_out - min_out)) / (max_in - min_in) + min_out;
+};
